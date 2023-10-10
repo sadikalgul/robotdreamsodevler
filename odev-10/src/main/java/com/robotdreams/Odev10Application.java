dee35704911ee14743700a3f4972a9c87ad6b0d2
sadikalgul <ytuloyal@gmail.com>
2023-10-10 21:16:09 +0300

    commit:10102023: odev10 added

---
+++ odev-10/src/main/java/com/robotdreams/Odev10Application.java
@@ -0,0 +1,13 @@
+package com.robotdreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Odev10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Odev10Application.class, args);
+    }
+
+}
